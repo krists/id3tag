ddaeb487a19aab8f773c30c727107a8907d20008
Krists Ozols <krists@ithouse.lv>
2012-12-29 17:54:20 +0200

    small refactor. can read v2 tag frames and padding

--- lib/id3tag/audio_file.rb
+++ lib/id3tag/audio_file.rb
@@ -23,5 +23,46 @@ module ID3Tag
       @file.seek(-ID3V1_TAG_SIZE + IDV1_TAG_IDENTIFIER.size, IO::SEEK_END)
       @file.read
     end
+
+    def v2_tag_body
+      @file.seek(v2_tag_frame_and_padding_position)
+      @file.read(v2_tag_frame_and_padding_size)
+    end
+
+    private
+
+    def v2_tag_frame_and_padding_position
+      ID3V2_TAG_HEADER_SIZE + v2_extended_header_size
+    end
+
+    def v2_tag_frame_and_padding_size
+      v2_tag_header.tag_size - v2_extended_header_size
+    end
+
+    def v2_extended_header_size
+      @v2_extended_header_size ||= get_v2_extended_header_size
+    end
+
+    def get_v2_extended_header_size
+      if v2_tag_header.extended_header?
+        SynchsafeInteger.decode(NumberUtils.convert_string_to_32bit_integer(v2_extended_header_size_bytes))
+      else
+        0
+      end
+    end
+
+    def v2_extended_header_size_bytes
+      @file.seek(ID3V2_TAG_HEADER_SIZE)
+      @file.read(4)
+    end
+
+    def v2_tag_header
+      @v2_tag_header ||= get_v2_tag_header
+    end
+
+    def get_v2_tag_header
+      @file.rewind
+      ID3v2TagHeader.new(@file.read(ID3V2_TAG_HEADER_SIZE))
+    end
   end
 end
---
+++ lib/id3tag/number_utils.rb
@@ -0,0 +1,13 @@
+module ID3Tag
+  module NumberUtils
+    FORMAT_FOR_32BIT_INTEGER = 'N'
+    def self.convert_string_to_32bit_integer(string)
+      integers = string.unpack(FORMAT_FOR_32BIT_INTEGER)
+      integers.first || raise(ArgumentError, "String: '#{string}' could not be decoded as 32-bit integer")
+    end
+
+    def self.convert_32bit_integer_to_string(integer)
+      [integer].pack(FORMAT_FOR_32BIT_INTEGER)
+    end
+  end
+end
