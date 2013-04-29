4dbf79b227c505d62dd9f9df03a301f5af7df9eb
Krists Ozols <krists@ithouse.lv>
2013-04-29 09:43:56 +0300

    refactor

---
+++ lib/id3tag/frame_id_advisor.rb
@@ -0,0 +1,50 @@
+module ID3Tag
+  class FrameIdAdvisor
+    COMMON_FRAME_IDS_BY_VERSION = {
+      'v1.x' => {
+        :artist => :artist,
+        :title => :title,
+        :album => :album,
+        :year => :year,
+        :comments => :comments,
+        :genre => :genre,
+        :track_nr => :track_nr
+      },
+      'v2.2' => {
+        :artist => :TP1,
+        :title => :TT2,
+        :album => :TAL,
+        :year => :TYE,
+        :comments => :COM,
+        :genre => :TCO,
+        :track_nr => :TRK
+      },
+      'v2.3' => {
+        :artist => :TPE1,
+        :title => :TIT2,
+        :album => :TALB,
+        :year => :TYER,
+        :comments => :COMM,
+        :genre => :TCON,
+        :track_nr => :TRCK
+      },
+      'v2.4' => {
+        :artist => :TPE1,
+        :title => :TIT2,
+        :album => :TALB,
+        :year => :TDRC,
+        :comments => :COMM,
+        :genre => :TCON,
+        :track_nr => :TRCK
+      }
+    }
+
+    def initialize(version, major_version)
+      @version, @major_version = version, major_version
+    end
+
+    def advise(frame_name)
+      COMMON_FRAME_IDS_BY_VERSION["v#{@version}.#{@major_version}"][frame_name]
+    end
+  end
+end
