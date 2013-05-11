6a62589fd83e9816fb5a8d654978443cedbc776d
Krists Ozols <krists@ithouse.lv>
2013-05-11 13:23:20 +0300

    string unsynchronisation added

--- spec/lib/id3tag/string_util_spec.rb
+++ spec/lib/id3tag/string_util_spec.rb
@@ -18,4 +18,32 @@ describe ID3Tag::StringUtil do
       it { should be_false }
     end
   end
+
+  describe "do_unsynchronization" do
+    let(:input) { }
+    subject { described_class.do_unsynchronization(input) }
+
+    context "when a false synchronization is present" do
+      let(:input) { "\xFF\xEE" }
+      it { should eq("\xFF\x00\xEE") }
+    end
+    context "when a false synchronization is not present" do
+      let(:input) { "\xEE\xEE" }
+      it { should eq("\xEE\xEE") }
+    end
+  end
+
+  describe "undo_unsynchronization" do
+    let(:input) { }
+    subject { described_class.undo_unsynchronization(input) }
+
+    context "when unsynchronization in present" do
+      let(:input) { "\xFF\x00\xEE\xEE" }
+      it { should eq("\xFF\xEE\xEE") }
+    end
+    context "when unsynchronization in not present" do
+      let(:input) { "\xEE\xEE" }
+      it { should eq("\xEE\xEE") }
+    end
+  end
 end
