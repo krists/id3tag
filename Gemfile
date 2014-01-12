3cb15303c43477df5a624a3e94da523a6663dec0
Krists Ozols <krists.ozols@tieto.com>
2014-01-12 19:27:34 +0200

    removed debugger from specs

--- Gemfile
+++ Gemfile
@@ -2,5 +2,3 @@ source "http://rubygems.org"
 
 # Specify your gem's dependencies in id3tag.gemspec
 gemspec
-# gem "pry-byebug"
-gem "byebug"
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,7 +10,6 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'rspec'
 require 'id3tag'
-require 'pry'
 
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
