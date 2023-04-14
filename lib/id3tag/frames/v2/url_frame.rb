1f260254718ba2590c3b05daf5105a1eff6a6a3d
Dom Christie <christiedom@gmail.com>
2023-04-14 09:07:08 +0100

    Simplify UrlFrame

--- lib/id3tag/frames/v2/url_frame.rb
+++ lib/id3tag/frames/v2/url_frame.rb
@@ -2,11 +2,7 @@ module ID3Tag
   module Frames
     module V2
       class UrlFrame < BasicFrame
-        def url
-          raw_content
-        end
-
-        alias_method :content, :url
+        alias_method :url, :content
         alias_method :inspectable_content, :content
       end
     end
